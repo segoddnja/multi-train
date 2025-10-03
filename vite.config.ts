300165cac307ef1cba217978033b7ca36673274d
Dmytro Zasiadko <dmytro@2amigos.us>
2025-10-03 14:26:48 +0200

    feat: make Vite base path conditional for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+const isGhPages = process.env.VITE_DEPLOY_TARGET === "gh-pages";
 export default defineConfig({
+  base: isGhPages ? "/multi-train/" : "/",
   plugins: [react(), tailwindcss()],
 });
